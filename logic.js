2fff710e1134afb07fc6ff562779bf631835544b
Amirhossein Kamrani <ceit.Engineer@gmail.com>
2020-03-25 23:43:14 +0330

    improvments

--- logic.js
+++ logic.js
@@ -2,33 +2,28 @@ var script = document.createElement('script');
 script.src = 'https://code.jquery.com/jquery-3.2.1.min.js';
 document.getElementsByTagName('head')[0].appendChild(script);
 
-// $.getJSON( "movies-information.json", function( data ) {
-//     console.log(data);
-//   });
+$.getJSON( "movies-information.json", function( data ) {
+    console.log(data);
+  });
 
-function loadJSON(callback) {
-
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'movies-information.json', true); // Replace 'my_data' with the path to your file
-    xobj.onreadystatechange = function () {
-        if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-        }
-    };
-    xobj.send(null);
-}
-
-function init() {
-    loadJSON(function (response) {
-        // Parse JSON string into object
-        var actual_JSON = JSON.parse(response);
-        console.log(actual_JSON);
-    });
-}
-
-loadJSON(init)
+// function loadJSON(callback) {
+//     var xobj = new XMLHttpRequest();
+//     xobj.overrideMimeType("application/json");
+//     xobj.open('GET', 'movies-information.json', true);
+//     xobj.onreadystatechange = function () {
+//         if (xobj.readyState == 4 && xobj.status == "200") {
+//             callback(xobj.responseText);
+//         }
+//     };
+//     xobj.send(null);
+// }
+// function init() {
+//     loadJSON(function (response) {
+//         var actual_JSON = JSON.parse(response);
+//         console.log(actual_JSON);
+//     });
+// }
+// loadJSON(init)
 
 function openWin(movieURL, posterURL = "https://inspirecast.ca/wp-content/uploads/2016/01/Robert-Kiyosaki-The-only-difference-between-a-rich-person-and-poor-person-is-how-they-use-their-time.jpg") {
     var myWindow = window.open("MOVIE");
