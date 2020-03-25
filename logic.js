1a876021d0db0178a9d79814d0c2e3581d40cf09
Amirhossein Kamrani <ceit.Engineer@gmail.com>
2020-03-26 02:46:55 +0330

    improvments

--- logic.js
+++ logic.js
@@ -19,7 +19,7 @@ function movieExtractor(response) {
     var movies = JSON.parse(response);
     for (var i = movies.length - 1; i >= 0 ; i--) {
         var movieCard =
-            `<!-- Movie Card --><div class='card persian' onclick='openWin("${movies[i].URL}")'><img src = ${movies[i].cover} class='movie-cover' ><div class='overlay'><div class='text'><h3>خلاصه داستان</h3><hr><h4>${movies[i].brief}</h4><hr><button class='card-button trailer-button'>تریلر<i class='fas fa-film button-icon-with-text'></i></button><button class='card-button favorite-button'><i class='far fa-heart button-icon-without-text'></i></button></div></div><div class='container center-text'><h4><b>${movies[i].name}</b></h4><h4>دوبله - ${movies[i].rate} - ${movies[i].year}</h4><h4>${movies[i].genre}</h4></div></div>`;
+            `<!-- Movie Card --><div class='card persian' onclick='openWin("${movies[i].URL}")'><img src = ${movies[i].cover} class='movie-cover' ><div class='overlay'><div class='text'><h3>خلاصه داستان</h3><hr><h4>${movies[i].brief}</h4><hr><button class='card-button trailer-button'>تریلر<i class='fas fa-film button-icon-with-text'></i></button><button class='card-button favorite-button'><i class='far fa-heart button-icon-without-text'></i></button></div></div><div class='container center-text'><h4 class="movie-name"><b>${movies[i].name}</b></h4><h4>دوبله - ${movies[i].rate} - ${movies[i].year}</h4><h5>${movies[i].genre}</h5></div></div>`;
         $(".main").append(movieCard);
     }
     
