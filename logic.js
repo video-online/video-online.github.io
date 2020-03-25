3e3ef5697024aa8c078257ad5dd0ede137ac931d
Amirhossein Kamrani <ceit.Engineer@gmail.com>
2020-03-26 01:07:20 +0330

    improvments

--- logic.js
+++ logic.js
@@ -15,9 +15,10 @@ function movieExtractor() {
     console.log(movies);
     for (var i = 0; i < movies.length; i++) {
         console.log(movies[i]);
+        var movieCard =
+            "<!-- Movie Card --> <script>function togoMovie() {var myWindow = window.open('MOVIE');myWindow.document.write('<title>AK Movie</title><video src='http://s7.dlrozane.net/dlroozane/98/10/Togo.2019.1080p.Farsi.Dubbed.mkv' style='width:100%;height:100%' poster='https://inspirecast.ca/wp-content/uploads/2016/01/Robert-Kiyosaki-The-only-difference-between-a-rich-person-and-poor-person-is-how-they-use-their-time.jpg' controls=''></video>');}</script><div class='card persian' onclick='togoMovie()'><img src = 'https://dlroozane.net/wp-content/uploads/2020/01/Togo.jpg' class='movie-cover' ><div class='overlay'><div class='text'><h3>خلاصه داستان</h3><hr><h4>این فیلم داستان یک سگ سورتمه‌ای به نام توگو را به تصویر می‌کشد که هدایت یک گروه از سگ‌هارا در دهه‌ی بیست میلادی بر عهده دارد ، اما اکثر افراد هدایت چنین سگ کوچک و ضعیفی راباورنکردنی می‌دانند و…</h4><hr><button class='card-button trailer-button'>تریلر<i class='fas fa-film button-icon-with-text'></i></button><button class='card-button favorite-button'><i class='far fa-heart button-icon-without-text'></i></button></div></div><div class='container center-text'><h4><b>توگو</b></h4><h4>دوبله - 8.2 - 1080p</h4><h4>ماجراجویی | درام</h4></div></div>";
+        $(".main").append(movieCard);
     }
-    // var movieCard = "<p>Text.</p>"; 
-    // $(".main").append(movieCard);
 }
 
 function openWin(movieURL, posterURL = "https://inspirecast.ca/wp-content/uploads/2016/01/Robert-Kiyosaki-The-only-difference-between-a-rich-person-and-poor-person-is-how-they-use-their-time.jpg") {
@@ -26,5 +27,5 @@ function openWin(movieURL, posterURL = "https://inspirecast.ca/wp-content/upload
 }
 
 
-loadJSON(init)
+loadJSON(movieExtractor)
 console.log(movies);
