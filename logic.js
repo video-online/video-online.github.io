71f0ddd5001d1ab257384c523ddd32c806c9f64a
Amirhossein Kamrani <ceit.Engineer@gmail.com>
2020-03-26 07:33:25 +0330

    improvments

--- logic.js
+++ logic.js
@@ -11,18 +11,19 @@ function loadJSON(callback) {
 }
 
 function openWin(movieURL, posterURL = "https://inspirecast.ca/wp-content/uploads/2016/01/Robert-Kiyosaki-The-only-difference-between-a-rich-person-and-poor-person-is-how-they-use-their-time.jpg") {
-    var myWindow = window.open("MOVIE");
-    myWindow.document.write(`<title>AK Movie</title><video src="${movieURL}" style="width:100%;height:100%" poster="${posterURL}" controls=""></video>`);
+    // var myWindow = window.open("MOVIE");
+    // myWindow.document.write(`<title>AK Movie</title><video src="${movieURL}" style="width:100%;height:100%" poster="${posterURL}" controls=""></video>`);
+    $('.ui.modal').modal('show');
 }
 
 function movieExtractor(response) {
     var movies = JSON.parse(response);
-    for (var i = movies.length - 1; i >= 0 ; i--) {
+    for (var i = movies.length - 1; i >= 0; i--) {
         var movieCard =
             `<!-- Movie Card --><div class='card persian' onclick='openWin("${movies[i].URL}")'><img src = ${movies[i].cover} class='movie-cover' ><div class='overlay'><div class='text'><h3>خلاصه داستان</h3><hr><h4>${movies[i].brief}</h4><hr><button class='card-button trailer-button'>تریلر<i class='fas fa-film button-icon-with-text'></i></button><button class='card-button favorite-button'><i class='far fa-heart button-icon-without-text'></i></button></div></div><div class='container center-text'><h4 class="movie-name"><b>${movies[i].name}</b></h4><h4>دوبله - ${movies[i].rate} - ${movies[i].year}</h4><h5>${movies[i].genre}</h5></div></div>`;
         $(".movies").append(movieCard);
     }
-    
+
     $(document).ready(function () {
         $('body').persianNum();
     })
